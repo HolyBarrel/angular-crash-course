d7aabb2848740baea3726aa92f784c57166f5a32
HolyBarrel <magnus.allison@getmail.no>
2025-06-09 16:01:58 +0200

    Added basic components - header and home (26:15)

--- angular-app/src/app/app.ts
+++ angular-app/src/app/app.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <app-header/>
+
+    <main> 
+      <app-home/> 
+    </main>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [`
+    p {
+      font-size: 1.2em;
+      color: #663;
+    }
+
+    main {
+      padding: 16px;
+    }
+    `],
 })
 export class App {
   protected title = 'angular-app';
