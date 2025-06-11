fb577f6b96c9933dd35b17280062d47cad9eeb73
HolyBarrel <magnus.allison@getmail.no>
2025-06-11 12:25:22 +0200

    Implemented use of ng routing

--- angular-app/src/app/app.ts
+++ angular-app/src/app/app.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header],
+  imports: [RouterOutlet, Header],
   template: `
     <app-header/>
 
-    <main> 
-      <app-home/> 
+    <main>
+      <router-outlet />
     </main>
-
-    <router-outlet />
   `,
   styles: [`
     p {
